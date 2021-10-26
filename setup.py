df509a70146fc381bc9b211e15b6b931a4e81946
justinhyon <justinhyon@gmail.com>
2021-10-26 14:38:34 -0700

    update setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="teamflow_placeholder_name",
+    version="0.0.1",
+    author="Justin Hyon",
+    author_email="justinhyon@gmail.com",
+    description="package for inter brain realtime EEG experiments.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/justinhyon/teamflow",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3-Alpha",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
